74edb57ec674412065cc3d0d9eb9db9e58443ac7
Denis Koinash <d.koinash@cyclone-nov.ru>
2024-02-01 13:20:42 +0300

    Реализация тестов

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,13 +4,33 @@
 
 #include <gtest/gtest.h>
 
-// TEST_F(TestCache, Overflow)
-TEST(TestCache, Overflow)
-{
-  const uint64_t COUNT = 5;
 
+TEST(Converter, ToMilliWithMS)
+{
   uint64_t value = 1706278376987;
   milliseconds ms(value);
   std::string sample = Converter::ToString(ms, true);
   ASSERT_EQ(Converter::ToMilli(sample).count(), value);
+}
+
+TEST(Converter, ToMilliWithoutMS)
+{
+  uint64_t value = 1706278376000;
+  milliseconds ms(value);
+  std::string sample = Converter::ToString(ms, false);
+  ASSERT_EQ(Converter::ToMilli(sample).count(), value);
+}
+
+TEST(Converter, ToStringWithMS)
+{
+  std::string str = "20211203_122435898";
+  milliseconds ms = Converter::ToMilli(str);
+  ASSERT_EQ(Converter::ToString(ms, true), str);
+}
+
+TEST(Converter, ToStringWithoutMS)
+{
+  std::string str = "20211203_122435";
+  milliseconds ms = Converter::ToMilli(str);
+  ASSERT_EQ(Converter::ToString(ms, false), str);
 }
